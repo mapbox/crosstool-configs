9570a53ecf1c7177d08236b534a8ef2975bdce00
Konstantin Käfer <mail@kkaefer.com>
2015-10-30 12:39:13 +0100

    proper separation between build and host platforms

--- build.sh
+++ build.sh
@@ -1,12 +1,28 @@
 #!/usr/bin/env bash
 
-if [ -z "$1" ]; then
-    echo "You must specify a toolchain name"
+if [ `uname -s` == 'Darwin' ]; then
+    CROSSTOOL_BUILD_PLATFORM=osx-`uname -m`
+elif [ `uname -s` == 'Linux' ]; then
+    CROSSTOOL_BUILD_PLATFORM=linux-`uname -m`
+else
+    echo "Error: '`uname -s`' is not a supported platform"
+    exit 1
+fi
+
+if [ ! -d configs/${CROSSTOOL_BUILD_PLATFORM} ]; then
+    echo "Error: There are no scripts available for '${CROSSTOOL_BUILD_PLATFORM}'. The build platforms are:"
+    for i in configs/* ; do echo \* $(basename $i) ; done
+    exit 1
+fi
+
+if [ -z "$1" -o ! -d "configs/${CROSSTOOL_BUILD_PLATFORM}/$1" ]; then
+    echo "Error: You must specify a host platform. The host platforms for '${CROSSTOOL_BUILD_PLATFORM}' are"
+    for i in configs/${CROSSTOOL_BUILD_PLATFORM}/* ; do echo \* $(basename $i) ; done
     exit 1
 fi
 
 export CROSSTOOL_ROOT_FOLDER=`pwd`
-export CROSSTOOL_TOOLCHAIN_NAME=$1
+export CROSSTOOL_TOOLCHAIN_NAME=${CROSSTOOL_BUILD_PLATFORM}/$1
 
 ulimit -n 1024
 
